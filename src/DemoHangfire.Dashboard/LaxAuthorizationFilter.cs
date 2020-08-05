b0d1273e70b7c768fa8c9519d74872f7cb8485b6
Xavier Dervaux <dex@smart.coop>
2020-08-05 17:02:25 +0200

    Add hangfire dashboard

---
+++ src/DemoHangfire.Dashboard/LaxAuthorizationFilter.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire.Dashboard;
+
+namespace DemoHangfire.Dashboard
+{
+    public class LaxAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            //This is unsafe, make sure this app is not used exposed to the world.
+            return true;
+        }
+    }
+}
